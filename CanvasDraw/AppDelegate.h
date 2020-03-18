dfa212ad9069c2e1cb823862dd82a7090463ab25
DonMag <demagnus@ra.rockwell.com>
2020-03-18 08:55:58 -0400

    Initial commit

---
+++ CanvasDraw/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CanvasDraw
+//
+//  Created by Don Mag on 3/18/20.
+//  Copyright © 2020 Don Mag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ CanvasDraw/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  CanvasDraw
+//
+//  Created by Don Mag on 3/18/20.
+//  Copyright © 2020 Don Mag. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+
+@end
